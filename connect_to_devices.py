f3158da04df513c80385e8e762b4b26d29f117d1
Madhu Sharma <madhshar@cisco.com>
2018-03-12 00:02:03 -0400

    first draft

---
+++ connect_to_devices.py
@@ -0,0 +1,56 @@
+"""
+I have 2 solutions for this problem.
+1. Use netmiko and Asyncio to set up concurrent sessions.
+2. Use parallel-ssh, which is a python package, written for the same purpose. Unfortunately this package doesn't work well when different usernames
+   and passwords are used. So for the sake of this exercise, let us assume all the devices use the same user/pass combination.
+"""
+import json, asyncio
+from device import Device
+from pssh.pssh_client import ParallelSSHClient
+device_dict = {}
+with open('device_data.json', 'r') as f:
+    json_out = json.loads(f.read())
+
+def connect_using_async():
+    """
+    This method uses netmiko to connect to SSH to the servers.
+    To run mulitple SSH sessions concurrently, I used asyncio
+    """
+    for item in json_out['device_list']:
+        device_dict['%s' %(item['hostname'])] = Device(
+                address = item['address'],
+                host = item['hostname'],
+                username = item['username'],
+                password = item['password'],
+                port = item['port'],
+                commands = json_out['commands']
+            )
+            
+    @asyncio.coroutine
+    def my_coroutine(server,*args):
+        print('{0} Connecting ...'.format(server.host))
+            #print('{0} sleeping for: {1} seconds'.format(task_name, seconds_to_sleep))
+        server.execute()
+        yield from asyncio.sleep(5)
+    
+    loop = asyncio.get_event_loop()
+    tasks = [my_coroutine(v) for k,v in device_dict.items()]
+    loop.run_until_complete(asyncio.wait(tasks))
+    loop.close()
+
+def connect_using_pssh():
+    """
+    This method uses parallel-ssh package to connect to the devices
+    """
+    device_list = [item['address'] for item in json_out['device_list']]
+    user = set([item['username'] for item in json_out['device_list']]).pop()
+    password = set([item['password'] for item in json_out['device_list']]).pop()
+    client = ParallelSSHClient(device_list, user=user, password=password)
+    output = client.run_command('whoami')
+    for host, host_output in output.items():
+        for line in host_output:
+                print(line)
+
+if __name__=='__main__':
+    connect_using_async()
+    connect_using_pssh()
---
+++ parse_logfile.py
@@ -0,0 +1,66 @@
+import argparse,re,datetime
+from time import strptime
+import pytz
+strptime('Feb','%b').tm_mon
+
+def parse_logfile(file,key):
+    """
+    Reads the log file
+    parses the logfile for the key
+    counts the number of line where the key appears in the log file
+    Computes the mean between each timestamp
+    finally converts all the time stamps from UTC to PST
+    """
+
+    count = 0
+    datetimeFormat = '%m %d %H:%M:%S'
+    datetimeFormat1 = '%H:%M:%S'
+    diff = 0 
+    temp = None
+    regex = re.compile('(\d+:\d+:\d+)\s+')
+    old_timezone = pytz.timezone("GMT")
+    new_timezone = pytz.timezone("US/Pacific")
+
+    with open(file,'r') as f:
+        for line in f:
+            if re.search(key.lower(),line.lower()):
+                count += 1
+                match_object = re.search('([a-zA-Z]{1,4})\s+(\d+)\s+(\d+:\d+:\d+)',line)
+                if match_object:
+                    time_stamp = '%s %s %s' %(
+                        strptime(match_object.group(1), '%b').tm_mon,
+                        match_object.group(2),
+                        match_object.group(3),
+                    )
+                    if temp:
+                        diff += (datetime.datetime.strptime(time_stamp, datetimeFormat) - \
+                        datetime.datetime.strptime(temp, datetimeFormat)).total_seconds()
+                        print (diff)
+                    temp = time_stamp             
+                    new_line = regex.sub(\
+                        lambda m: m.group().replace(m.group(0), '%s ' %(old_timezone.localize(datetime.datetime.strptime(\
+                        m.group(1), datetimeFormat1)).astimezone(new_timezone).strftime('%H:%M:%S'))), line)  
+                    new_line = re.sub('\+[0-9]{1,5}', '-8000', new_line)     
+                    print (new_line)
+
+    print ('count is %s' %count)
+    print ('The average delay between each log occurence is %s' %float(diff/(count-1)))
+
+if __name__=='__main__':
+    """
+    usage: parse_logfile.py [-h] key filename
+
+    Enter a keyword
+
+    positional arguments:
+    key         keyword to parse
+    filename    location of the logfile
+    """
+    parser = argparse.ArgumentParser(description='Enter a keyword')
+    parser.add_argument('key', 
+                    help='keyword to parse')
+    parser.add_argument('filename', 
+                    help='location of the logfile')
+    args = parser.parse_args()
+    parse_logfile(args.filename,args.key)
+
